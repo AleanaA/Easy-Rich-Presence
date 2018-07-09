c2228de21235f9ed4dfda3b187c10189da44c21d
Conner May <aleanaazure@gmail.com>
2018-07-09 06:33:29 -0400

    Add all options

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const client = require('discord-rich-presence')('id');
 client.updatePresence({
   details: "First line",
   state: "Second line",
-  largeImageKey: 'image',
+  largeImageKey: 'largeimage',
+  largeImageText: "Large Image Hover Text",
+  smallImageKey: 'smallimage',
+  smallImageText: "Small Image Hover Text",
   instance: true,
 });
